b20f82c7fb63dbf764b9b85b93407fb9fecae21e
rupambora31 <iamrupam99@gmail.com>
2023-12-29 18:44:50 +0530

    install react and react-dom via npm

--- EP-02/app.js
+++ EP-02/app.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
   'div',
   { id: 'parent' },
   [
     React.createElement('div', { id: 'child1' }, [
-      React.createElement('h1', {}, 'Hi from h1 tag!'),
-      React.createElement('h2', {}, 'Hi from h2 tag!'),
+      React.createElement('h1', {}, 'This is namaste React 🚀'),
+      React.createElement('h2', {}, 'by Akshay.'),
     ]),
     React.createElement('div', { id: 'child2' }, [
       React.createElement('h1', {}, 'Hi from h1 tag!'),
