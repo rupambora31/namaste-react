83a575e51c526ef30ec762371be83c44780a1d4d
rupambora31 <iamrupam99@gmail.com>
2023-12-31 10:34:45 +0530

    add RestaurantCard component in body

--- EP-04/app.js
+++ EP-04/app.js
@@ -9,6 +9,8 @@ import ReactDOM from 'react-dom/client';
  *    -Search
  *    -RestaurantContainer
  *      -RestaurantCard
+ *        -Img
+ *        -Name, Rating, Cuisine, ETA, Price
  * Footer
  *    -Copyright
  *    -Links
@@ -36,10 +38,44 @@ const Header = () => {
     </div>
   );
 };
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: '#f0f0f0' }}>
+      <img
+        className="res-logo"
+        alt="restaurant logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rzfjjqd53rek0rgot0zd"
+      />
+      <h2>JBs</h2>
+      <div style={{fontWeight:"600"}}>⭐ 4.5  •  <span>43 mins</span></div>
+      <p>Sweets, North Indian, South Indian</p>
+      
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
