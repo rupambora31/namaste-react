12652232336abba9945b0ae6d787b32e0523fd7e
rupambora31 <iamrupam99@gmail.com>
2024-01-03 10:21:42 +0530

    fetch and render data from Swiggy API

---
+++ EP06/src/components/Body.js
@@ -0,0 +1,51 @@
+import RestaurantCard from './RestaurantCard';
+import { useEffect, useState } from 'react';
+const Body = () => {
+  // State-Variable
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1157917&lng=91.7085933&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    // Optional-Chaining
+    setListOfRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // filter logic here
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.4
+            );
+            setListOfRestaurant(filteredList);
+            console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ EP06/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const CDN_URL =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
+
+export const LOGO_URL =
+  'https://img.freepik.com/premium-vector/healthy-food-logo_476121-102.jpg';
+
+// Named-Export
