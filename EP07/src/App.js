c601adda879d801a551f18af64bc8d4b8e29bccc
rupambora31 <iamrupam99@gmail.com>
2024-01-05 18:34:00 +0530

    create children-routes and use Outlet

--- EP07/src/App.js
+++ EP07/src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Contact from './components/Contact';
 import Error from './components/Error';
 
@@ -11,7 +11,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -20,15 +20,21 @@ const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Body />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/contact',
-    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
